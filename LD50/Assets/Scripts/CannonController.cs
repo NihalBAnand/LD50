5d4cae503f38ac6deaa628f6a01d5912b78b2a85
Anish <38837666+ansaws@users.noreply.github.com>
2022-04-02 14:05:30 -0500

    added gunpowder depletion
    need to still add the change to enemy spawn chance
    and then begin on mini game for cannon

--- LD50/Assets/Scripts/CannonController.cs
+++ LD50/Assets/Scripts/CannonController.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class CannonController : MonoBehaviour
 {
     // Start is called before the first frame update
+    public bool cannonStatus;
+    public float gunpowderStack;
+    public float depletionRate;
     void Start()
     {
-        
+        cannonStatus = true;
+        gunpowderStack = 100;
+        depletionRate = .1f;
+        StartCoroutine(depleteCoroutine());
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (gunpowderStack< 1)
+        {
+            cannonStatus = false;
+            gunpowderStack = 0;
+            Debug.Log("NO GUNPOWDER!!!");
+        }
+        else
+        {
+            cannonStatus = true;
+        }
         
     }
+    IEnumerator depleteCoroutine()
+    {
+        while (gunpowderStack>0)
+        {
+            gunpowderStack *= 1 - depletionRate;
+            yield return new WaitForSeconds(1.0f);
+            
+        }
+    }
+
 }
