f2b70325f8d016d43d85460697095d8d0f154956
TheFoppian <38819438+TheFoppian@users.noreply.github.com>
2022-04-02 18:01:47 -0500

    Added enemy spawning mechanics

--- LD50/Assets/Scripts/ShipController.cs
+++ LD50/Assets/Scripts/ShipController.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class ShipController : MonoBehaviour
 {
     // Start is called before the first frame update
-    public float enemySpawnChance = .01f;
+    public float enemySpawnChance = .1f;
 
+    public GameObject enemy;
 
+    public GameObject rightSideSpawn;
+    public GameObject leftSideSpawn;
     void Start()
     {
+        rightSideSpawn = GameObject.Find("RightSpawnLocation");
+        leftSideSpawn = GameObject.Find("LeftSpawnLocation");
 
+        StartCoroutine(spawnEnemies());
     }
 
     // Update is called once per frame
@@ -21,6 +27,27 @@ public class ShipController : MonoBehaviour
 
     private IEnumerator spawnEnemies()
     {
-        yield return null;
+        while (true)
+        {
+            yield return new WaitForSeconds(2);
+            int spawnAttempt = Random.Range(0, 101);
+            if (spawnAttempt <= 100 * enemySpawnChance)
+            {
+                int numSpawned = Random.Range(1, 6);
+                for (int i = 0; i < numSpawned; i++)
+                {
+                    GameObject newEnemy = Instantiate(enemy);
+                    bool rightSide = Random.Range(0, 2) == 1;
+                    if (rightSide)
+                    {
+                        newEnemy.transform.position = rightSideSpawn.transform.position;
+                    }
+                    else
+                    {
+                        newEnemy.transform.position = leftSideSpawn.transform.position;
+                    }
+                }
+            }
+        }
     }
 }
