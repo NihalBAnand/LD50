ecbecba7f300aa7ad53bcec72ee98fa5abe8b5fa
Anish <38837666+ansaws@users.noreply.github.com>
2022-04-02 18:02:30 -0500

    cannon mini game progress

--- LD50/Assets/Scripts/CannonController.cs
+++ LD50/Assets/Scripts/CannonController.cs
@@ -10,10 +10,27 @@ public class CannonController : InteractableGeneric
     public int enemySpawnFactor;
     public GameObject ship;
 
+    private bool activated;
+
     public override void Interaction(GameObject player)
     {
-
+        if (!activated)
+        {
+            activated = true;
+            Camera.main.transform.parent = GameObject.Find("Center").transform;
+            Camera.main.transform.position = GameObject.Find("GunpowderStorage").transform.position;
+            Camera.main.transform.position += new Vector3(0, 0, -10);
+        }
+        else
+        {
+            activated = false;
+            Camera.main.transform.parent = GameObject.FindGameObjectWithTag("Player").transform;
+            Camera.main.transform.position = GameObject.Find("Player").transform.position;
+            Camera.main.transform.position += new Vector3(0, 0, -10);
+            GameObject.Find("Player").GetComponent<PlayerController>().interacting = false;
+        }
     }
+
 
     void Start()
     {
@@ -22,6 +39,7 @@ public class CannonController : InteractableGeneric
         depletionRate = .1f;
         enemySpawnFactor = 2;
         StartCoroutine(depleteCoroutine());
+        activated = false;
 
     }
 
