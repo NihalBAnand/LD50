5a8f65be96f428522d09a8e24f6780d164513327
Anish <38837666+ansaws@users.noreply.github.com>
2022-04-03 13:46:23 -0500

    something

---
+++ LD50/Assets/Scripts/EntryController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntryController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    string[] Entrys = { "The situation is grim. Far more grim than I ever imagined it was possible to be. The cannons have started firing at things, things I can only see the vague shape of in these dark and choppy waters. I fear for my life, and I fear for <i>The Inevitable</i>. God help me..." };
+    void Start()
+    {
+             
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
