bae652e3771afefaed0c07a7d172296d5c339b9f
Anish <38837666+ansaws@users.noreply.github.com>
2022-04-03 13:12:31 -0500

    got logbook to display properly

--- LD50/Assets/Scripts/LogController.cs
+++ LD50/Assets/Scripts/LogController.cs
@@ -6,29 +6,29 @@ public class LogController : InteractableGeneric
 {
 
         private bool activated;
+        public GameObject log; 
         public override void Interaction(GameObject player)
         {
             if (!activated)
             {
                 activated = true;
-                Camera.main.transform.parent = GameObject.Find("Center").transform;
-                Camera.main.transform.position = GameObject.Find("LogIndicator").transform.position;
-                Camera.main.transform.position += new Vector3(0, 0, -10);
-            }
+                log.SetActive(true);
+
+        }
             else
             {
                 activated = false;
-                Camera.main.transform.parent = GameObject.FindGameObjectWithTag("Player").transform;
-                Camera.main.transform.position = GameObject.Find("Player").transform.position;
-                Camera.main.transform.position += new Vector3(0, 0, -10);
-                GameObject.Find("Player").GetComponent<PlayerController>().interacting = false;
-            }
+                log.SetActive(true);
+        }
         }
 
         // Start is called before the first frame update
         void Start()
         {
             activated = false;
+            log = GameObject.Find("Log");
+            log.SetActive(false);
+            
         }
 
         // Update is called once per frame
