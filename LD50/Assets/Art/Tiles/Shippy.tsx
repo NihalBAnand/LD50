eca15cca74fc92121597f43a808386d7cc36d09a
TheFoppian <38819438+TheFoppian@users.noreply.github.com>
2022-04-03 14:59:25 -0500

    Added ship tiles

---
+++ LD50/Assets/Art/Tiles/Shippy.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="Shippy" tilewidth="16" tileheight="16" tilecount="49" columns="7">
+ <image source="The_Bonny_Ship-20220402-203203.png" width="112" height="112"/>
+</tileset>
--- LD50/Assets/Scripts/ColumnTrigger.cs
+++ LD50/Assets/Scripts/ColumnTrigger.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class ColumnTrigger : MonoBehaviour
 {
+    private GameObject parent;
     // Start is called before the first frame update
     void Start()
     {
-        
+        parent = transform.parent.gameObject;
     }
 
     // Update is called once per frame
@@ -19,13 +21,18 @@ public class ColumnTrigger : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         //Move in front of player if the trigger is tripped
-        transform.parent.gameObject.GetComponent<SpriteRenderer>().sortingLayerName = "Objects-Front";
-        Debug.Log(transform.parent.gameObject.GetComponent<SpriteRenderer>().rendererPriority);
+        if (parent.GetComponent<SpriteRenderer>() != null)
+            parent.GetComponent<SpriteRenderer>().sortingLayerName = "Objects-Front";
+        if (parent.GetComponent<TilemapRenderer>() != null)
+            parent.GetComponent<TilemapRenderer>().sortingLayerName = "Objects-Front";
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         //Move behind the player if the trigger is un-tripped
-        transform.parent.gameObject.GetComponent<SpriteRenderer>().sortingLayerName = "Objects-Behind";
+        if (parent.GetComponent<SpriteRenderer>() != null)
+            parent.GetComponent<SpriteRenderer>().sortingLayerName = "Objects-Behind";
+        if (parent.GetComponent<TilemapRenderer>() != null)
+            parent.GetComponent<TilemapRenderer>().sortingLayerName = "Objects-Behind";
     }
 }
